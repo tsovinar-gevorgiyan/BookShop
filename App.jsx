3eb77701fca6395459c9e47edb62e8ac88ddfba0
tsovinar-gevorgiyan <139646124+tsovinar-gevorgiyan@users.noreply.github.com>
2024-06-17 15:37:47 +0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,23 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { ProductList } from './component/ProductList'
 import { BasketList } from './component/BasketList'
 
 function App() {
+  const [book, setBook] = useState([])
   const [basket, setBasket] = useState([])
-  const [saleApplied, setSaleApplied] = useState(false)
+  //const [saleApplied, setSaleApplied] = useState(false)
+  const [total, setTotal] = useState(0)
 
-  const [products, setProducts] = useState([
-    {id:101, title: "Business", price: 400, photo: "https://m.media-amazon.com/images/I/51njCf7LElL._SX342_SY445_.jpg"},
-    {id:102, title: "Economics", price: 340, photo: "https://m.media-amazon.com/images/I/51BacaJcneL._SX342_SY445_.jpg"},
-    {id:103, title: "Politics", price: 420, photo: "https://m.media-amazon.com/images/I/71vlj9Vrs-L._SY522_.jpg"},
-    {id:104, title: "Religious", price: 780, photo: "https://m.media-amazon.com/images/I/51yy2SZWumL._SX342_SY445_.jpg"},
-    {id:105, title: "History", price: 670, photo: "https://m.media-amazon.com/images/I/51vid9LTFuL._SX342_SY445_.jpg"},
-    {id:106, title: "Movies", price: 940, photo: "https://m.media-amazon.com/images/I/51dNGV-pNfL._SX342_SY445_.jpg"}
-  ])  
 
+  useEffect(()=>{
+    fetch("http://localhost:3004/books")
+    .then(res => res.json())
+    .then(res=> {
+      setBook(res)
+    }), []
+})
+  
   const moveToCart = id =>{
-    const found = products.find(x => x.id === id)
+    const found = book.find(x => x.id === id)
     const foundInBasket = basket.find(x => x.id === id)
 
     if (foundInBasket ) {
@@ -49,19 +51,25 @@ function App() {
     setBasket(basket.map(x => 
      x.count >= 3 ? {...x, count: x.count-1} : x
      ))
-     setSaleApplied(true)
+     //setSaleApplied(true)
   }
+
+  useEffect(()=>{
+    setTotal(basket.reduce((acc, item) => acc + item.price * item.count, 0))
+  }, [basket])
   
   return (
     <>
       <div className = "row">
-        <ProductList items = {products} onMove = {moveToCart}/>
+        <ProductList items = {book} onMove = {moveToCart}/>
         <BasketList items = {basket} 
               onAdd = {addToCart} 
               onDown = {minusFromCart} 
               onDelete = {deleteFromCart} 
               onSale={onSale} 
-              setSaleApplied={saleApplied}/>
+              //setSaleApplied={saleApplied}
+              total = {total}
+              />
         
       </div>
     </>
